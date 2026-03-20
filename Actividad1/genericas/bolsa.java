b7b6f049c9947466a7d72c61e51b9653b8d4a13d
Antony <antony.coaguila@estuduante.ucsm.edu.pe>
2026-03-19 23:28:58 -0500

    quito package

---
+++ Actividad1/genericas/bolsa.java
@@ -0,0 +1,23 @@
+package genericas;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Bolsa<T> implements Iterable<T> {
+
+    private ArrayList<T> lista = new ArrayList<>();
+    private int limite = 5;
+
+    public void add(T objeto) {
+        if (lista.size() < limite) {
+            lista.add(objeto);
+        } else {
+            System.out.println("Bolsa llena");
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return lista.iterator();
+    }
+}
---
+++ Actividad1/genericas/chocolatina.java
@@ -0,0 +1,14 @@
+package genericas;
+
+public class Chocolatina {
+
+    private String marca;
+
+    public Chocolatina(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+}
