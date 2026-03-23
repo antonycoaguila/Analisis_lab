478b45afa9beefc9a94d519591694ac5fc907fd6
Antony <antony.coaguila@estuduante.ucsm.edu.pe>
2026-03-22 23:10:28 -0500

    Agrego Lab02

--- Actividad1/Principal.java
+++ Actividad1/Principal.java
@@ -1,20 +1,32 @@
 package Actividad1;
+
 public class Principal {
 
     public static void main(String[] args) {
 
+
+        System.out.println(" Bolsa de Chocolatinas ");
+
         Bolsa<Chocolatina> bolsa = new Bolsa<>();
-
-        Chocolatina c1 = new Chocolatina("Milka");
-        Chocolatina c2 = new Chocolatina("Ferrero");
-        Chocolatina c3 = new Chocolatina("Sublime");
-
-        bolsa.add(c1);
-        bolsa.add(c2);
-        bolsa.add(c3);
+        
+        bolsa.add(new Chocolatina("Milka"));
+        bolsa.add(new Chocolatina("Ferrero"));
+        bolsa.add(new Chocolatina("Sublime"));
 
         for (Chocolatina c : bolsa) {
             System.out.println(c.getMarca());
+        }
+
+        System.out.println("\n Bolsa de Golosinas ");
+
+        Bolsa<Golosina> bolsa2 = new Bolsa<>();
+
+        bolsa2.add(new Golosina("Caramelo", 10));
+        bolsa2.add(new Golosina("Chicle", 5));
+        bolsa2.add(new Golosina("Gomita", 8));
+
+        for (Golosina g : bolsa2) {
+            System.out.println(g); // usa toString()
         }
     }
 }
